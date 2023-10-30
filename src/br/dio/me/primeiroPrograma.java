ae07c91e33b6b9baed47bdfa2a773fc3dd601e38
user <alefadonais5@gmail.com>
2023-10-30 18:47:14 -0300

    first commit

---
+++ src/br/dio/me/primeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.dio.me;
+
+import br.dio.me.model.Gato;
+
+public class primeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello world " + (a+b));*/
+
+    }
+}
